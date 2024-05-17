1c4183bff7196c0fdc8063159c2655fa5b5b7fd7
Kevin Gyori <gyorijonatan@gmail.com>
2024-05-17 15:18:55 +0100

    disable virtualization

--- src/components/Pane.tsx
+++ src/components/Pane.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  useTransition,
+} from "react";
+import { SetURLSearchParams, useSearchParams } from "react-router-dom";
+import "../wikipedia.css";
 
 async function fetchPage(title: string) {
   const url = `https://en.wikipedia.org/api/rest_v1/page/html/${title}`;
@@ -16,25 +24,122 @@ async function fetchPage(title: string) {
 }
 
 function WikiPage({
-  pageRef,
-  data,
+  body,
+  searchParams,
+  setSearchParams,
+  isInactive,
 }: {
-  pageRef: React.MutableRefObject<HTMLDivElement | null>;
-  data: string;
+  body: HTMLBodyElement | null;
+  searchParams: URLSearchParams;
+  setSearchParams: SetURLSearchParams;
+  isInactive: boolean;
 }) {
+  const styleLinks = useCallback(
+    (link: HTMLAnchorElement) => {
+      if (
+        searchParams
+          .getAll("wikiPage")
+          .includes(link.getAttribute("title") as string)
+      ) {
+        link.classList.add("bg-blue-200");
+      } else {
+        link.classList.remove("bg-blue-200");
+      }
+    },
+    [searchParams],
+  );
+
+  const addEventListener = useCallback(
+    (e: MouseEvent, link: HTMLAnchorElement) => {
+      // don't add an event listener multiple times
+      if (link.getAttribute("data-event-listener")) {
+        // console.log("event listener already added");
+        return;
+      }
+
+      // add attribute to prevent adding event listener multiple times
+      link.setAttribute("data-event-listener", "true");
+      // console.log("event listener added");
+
+      const title = link.getAttribute("title");
+
+      if (title !== null) {
+        e.preventDefault();
+        const index = searchParams.getAll("wikiPage").indexOf(title);
+        if (index < searchParams.getAll("wikiPage").length - 1) {
+          setSearchParams((prev) => {
+            const wikiPages = prev.getAll("wikiPage");
+            wikiPages.splice(index + 1, wikiPages.length - index - 1);
+            prev.delete("wikiPage");
+            wikiPages.forEach((wikiPage) => {
+              prev.append("wikiPage", wikiPage);
+            });
+            return prev;
+          });
+        }
+
+        setSearchParams((prev) => {
+          prev.append("wikiPage", title);
+          return prev;
+        });
+      }
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const handleBodyRender = useCallback(() => {
+    body?.querySelectorAll("a").forEach((link) => {
+      if (link.getAttribute("rel") !== "mw:WikiLink") {
+        // console.log("link isn't a wiki link", link.getAttribute("rel"));
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+        });
+        link.removeAttribute("href");
+        return;
+      }
+
+      styleLinks(link);
+    });
+    console.log("body rendered", body);
+    return body?.innerHTML ?? "";
+  }, [body, styleLinks]);
+
+  const renderedBody = useMemo(() => handleBodyRender(), [handleBodyRender]);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const current = ref.current?.querySelectorAll("a");
+    current?.forEach((link: HTMLAnchorElement) => {
+      link.addEventListener("click", (e) => addEventListener(e, link));
+    });
+
+    return () => {
+      current?.forEach((link: HTMLAnchorElement) => {
+        link.removeEventListener("click", (e) => addEventListener(e, link));
+      });
+    };
+  }, [addEventListener]);
+
   return (
-    <div
-      ref={pageRef}
-      className="overflow-hidden fade-in"
-      dangerouslySetInnerHTML={{ __html: data }}
-    />
+    <>
+      {!isInactive ? (
+        <div
+          ref={ref}
+          className="overflow-hidden fade-in"
+          dangerouslySetInnerHTML={{
+            __html: renderedBody,
+          }}
+        />
+      ) : null}
+    </>
   );
 }
 
 function WikiTitle({ title }: { title: string }) {
   return (
     <p
-      className="font-medium text-lg rotate-90 pb-1.5 w-screen origin-bottom-left"
+      className="font-medium text-lg rotate-90 pb-1.5 w-screen origin-bottom-left bg-white"
       dangerouslySetInnerHTML={{ __html: title }}
     />
   );
@@ -50,68 +155,10 @@ export function Pane({
   isInactive: boolean;
 }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const pageRef = useRef<HTMLDivElement>(null);
   const [pageTitle, setPageTitle] = useState("");
 
   const queryKey = useMemo(() => ["page", title], [title]);
   const fetchPageMemoized = useCallback(() => fetchPage(title), [title]);
-  const addEventListeners = useCallback(() => {
-    pageRef.current?.querySelectorAll("a").forEach((link) => {
-      if (link.getAttribute("rel") !== "mw:WikiLink") {
-        // console.log("link isn't a wiki link", link.getAttribute("rel"));
-        return;
-      }
-
-      if (
-        searchParams
-          .getAll("wikiPage")
-          .includes(link.getAttribute("title") as string)
-      ) {
-        link.classList.add("bg-blue-200");
-      } else {
-        link.classList.remove("bg-blue-200");
-      }
-
-      // don't add an event listener multiple times
-      if (link.getAttribute("data-event-listener")) {
-        // console.log("event listener already added");
-        return;
-      }
-
-      // add attribute to prevent adding event listener multiple times
-      link.setAttribute("data-event-listener", "true");
-      // console.log("event listener added");
-
-      link.addEventListener("click", (e) => {
-        if (link.querySelector("img")) {
-          e.preventDefault();
-          return;
-        }
-
-        const title = link.getAttribute("title");
-
-        if (title !== null) {
-          if (index < searchParams.getAll("wikiPage").length - 1) {
-            setSearchParams((prev) => {
-              const wikiPages = prev.getAll("wikiPage");
-              wikiPages.splice(index + 1, wikiPages.length - index - 1);
-              prev.delete("wikiPage");
-              wikiPages.forEach((wikiPage) => {
-                prev.append("wikiPage", wikiPage);
-              });
-              return prev;
-            });
-          }
-          e.preventDefault();
-
-          setSearchParams((prev) => {
-            prev.append("wikiPage", title);
-            return prev;
-          });
-        }
-      });
-    });
-  }, [searchParams, setSearchParams, index]);
 
   const { data, isPending, isError } = useQuery({
     queryKey: queryKey,
@@ -122,23 +169,37 @@ export function Pane({
     refetchOnMount: false,
     _optimisticResults: "optimistic",
   });
-  // const [pageState, setPageState] = useState<"bottom" | "middle" | "top">(
-  //   "top",
-  // );
+
+  const parseHTML = useCallback((data: string) => {
+    console.log("parsing html");
+    return new DOMParser().parseFromString(data, "text/html");
+  }, []);
+
+  const html = useMemo(() => parseHTML(data ?? ""), [data, parseHTML]);
 
   useEffect(() => {
-    // delete base url html tag from htmlPage
-    pageRef.current?.querySelector("base")?.remove();
-    setPageTitle(
-      (prev) => pageRef.current?.querySelector("title")?.textContent ?? prev,
-    );
-
-    addEventListeners();
-  }, [addEventListeners, data]);
+    console.log("setting page title");
+    const htmlTitle = html
+      .querySelector("head")
+      ?.querySelector("title")?.textContent;
+    setPageTitle((prev) => htmlTitle ?? prev);
+  }, [html]);
 
   const memoizedWikiPage = useMemo(
-    () => <WikiPage pageRef={pageRef} data={data || ""} />,
-    [data],
+    () => (
+      <WikiPage
+        body={html?.querySelector("body")}
+        searchParams={searchParams}
+        setSearchParams={setSearchParams}
+        isInactive={isInactive}
+      />
+    ),
+    [html, searchParams, setSearchParams, isInactive],
+  );
+
+  const memoizedWikiTitle = useMemo(
+    () => <WikiTitle title={pageTitle} />,
+    [pageTitle],
   );
 
   function closePane() {
@@ -174,15 +235,14 @@ export function Pane({
           style={{ zIndex: index, right: index * 40 }}
         >
           <button onClick={closePane}>X</button>
-          {!isPending ? <WikiTitle title={pageTitle} /> : " "}
-          {/* {isInactive ? "inactive" : "active"} */}
+          {!isPending ? memoizedWikiTitle : " "}
         </div>
         <div className="h-[calc(100vh-20px)] py-3 pr-3 scroll-y overflow-y-scroll overflow-x-hidden min-w-[650px] w-[650px] scrollbar-thin">
           <div
             className="text-2xl font-bold"
             dangerouslySetInnerHTML={{ __html: pageTitle }}
           ></div>
-          {!isInactive ? memoizedWikiPage : " "}
+          {memoizedWikiPage}
         </div>
       </div>
     );
--- src/components/Panes.tsx
+++ src/components/Panes.tsx
@@ -17,60 +17,60 @@ function WikiPanes({
   const pageRef = useRef<(HTMLDivElement | null)[]>([]);
 
   const [inactivePanes, setInactivePanes] = useState<Array<number | null>>([]);
-  const size = useWindowSize();
+  // const size = useWindowSize();
 
-  const handleScroll = useCallback(() => {
-    // Loop through each pane element and get its bounding rect
-    if (!pageRef.current) return;
-    const OFFSET = 40;
-    pageRef.current.forEach((pane, index) => {
-      const current = pane?.getBoundingClientRect();
-      // check if the pane is the first or last
-      // if so, then we don't need to check for prev or next
-      // if not, then we can check for prev and next
-      let prev, next;
-      if (index !== 0) {
-        prev = pageRef.current[index - 1]?.getBoundingClientRect();
-      }
-      if (index !== memoizedSearchParams.length - 1) {
-        next = pageRef.current[index + 1]?.getBoundingClientRect();
-      }
+  // const handleScroll = useCallback(() => {
+  //   // Loop through each pane element and get its bounding rect
+  //   if (!pageRef.current) return;
+  //   const OFFSET = 40;
+  //   pageRef.current.forEach((pane, index) => {
+  //     const current = pane?.getBoundingClientRect();
+  //     // check if the pane is the first or last
+  //     // if so, then we don't need to check for prev or next
+  //     // if not, then we can check for prev and next
+  //     let prev, next;
+  //     if (index !== 0) {
+  //       prev = pageRef.current[index - 1]?.getBoundingClientRect();
+  //     }
+  //     if (index !== memoizedSearchParams.length - 1) {
+  //       next = pageRef.current[index + 1]?.getBoundingClientRect();
+  //     }
 
-      // calculate the offset of the pane
-      // if (current?.x === OFFSET * index) {
-      //   console.log(`Pane ${index + 1}:`, "is docked to the left");
-      // }
+  //     // calculate the offset of the pane
+  //     // if (current?.x === OFFSET * index) {
+  //     //   console.log(`Pane ${index + 1}:`, "is docked to the left");
+  //     // }
 
-      const prevIsNear =
-        prev?.width > size.width - prev?.x + (OFFSET + 20) * index;
-      const nextIsNear = next?.x - 100 < OFFSET * (index + 1) + current?.width;
-      console.log(!nextIsNear, index);
-      if (next?.x === OFFSET * (index + 1)) {
-        // console.log("Next pane is docked to the left");
-        // console.log(`Pane ${index + 1}:`, "should be hidden");
-        setInactivePanes((prev) => [...prev, index]);
-      } else if (prevIsNear) {
-        // console.log(`Pane ${index + 1}:`, "is docked to the right");
-        setInactivePanes((prev) => [...prev, index]);
-      } else {
-        console.log(`Pane ${index + 1}:`, "is active");
-        setInactivePanes((prev) => prev.filter((pane) => pane !== index));
-      }
-      // console.log(`Pane ${index + 1}:`, next);
-      // console.log("size", size);
-    });
-  }, [memoizedSearchParams, size.width]);
+  //     const prevIsNear =
+  //       prev?.width > size.width - prev?.x + (OFFSET + 20) * index;
+  //     const nextIsNear = next?.x - 100 < OFFSET * (index + 1) + current?.width;
+  //     // console.log(!nextIsNear, index);
+  //     if (next?.x === OFFSET * (index + 1)) {
+  //       // console.log("Next pane is docked to the left");
+  //       // console.log(`Pane ${index + 1}:`, "should be hidden");
+  //       setInactivePanes((prev) => [...prev, index]);
+  //     } else if (prevIsNear) {
+  //       // console.log(`Pane ${index + 1}:`, "is docked to the right");
+  //       setInactivePanes((prev) => [...prev, index]);
+  //     } else {
+  //       // console.log(`Pane ${index + 1}:`, "is active");
+  //       setInactivePanes((prev) => prev.filter((pane) => pane !== index));
+  //     }
+  //     // console.log(`Pane ${index + 1}:`, next);
+  //     // console.log("size", size);
+  //   });
+  // }, [memoizedSearchParams, size.width]);
 
-  useEffect(() => {
-    // Attach scroll event listener
-    const ref = parentRef.current;
-    ref?.addEventListener("scroll", handleScroll);
+  // useEffect(() => {
+  //   // Attach scroll event listener
+  //   const ref = parentRef.current;
+  //   ref?.addEventListener("scroll", handleScroll);
 
-    // Detach scroll event listener on cleanup
-    return () => {
-      ref?.removeEventListener("scroll", handleScroll);
-    };
-  }, [memoizedSearchParams, parentRef, handleScroll]);
+  //   // Detach scroll event listener on cleanup
+  //   return () => {
+  //     ref?.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [memoizedSearchParams, parentRef, handleScroll]);
 
   const memoizedPanes = useMemo(
     () =>
@@ -88,8 +88,8 @@ function WikiPanes({
           <Pane
             title={title}
             index={key}
-            isInactive={inactivePanes.includes(key)}
-            // isInactive={false}
+            // isInactive={inactivePanes.includes(key)}
+            isInactive={false}
           />
         </div>
       )),
