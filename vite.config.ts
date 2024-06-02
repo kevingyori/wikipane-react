1a1f66061a40cc864190e5a214ef6ab63980cbc6
Kevin Gyori <gyorijonatan@gmail.com>
2024-06-02 17:44:27 +0100

    enable compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// const ReactCompilerConfig = {
-//   sources: (filename: string | string[]) => {
-//     return filename.indexOf("src") !== -1;
-//   },
-// };
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ["babel-plugin-react-compiler"]],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
 });
