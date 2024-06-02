41f249e1cdef3e1d39e9d6edb06a1ceb1ed0406b
Kevin Gyori <gyorijonatan@gmail.com>
2024-06-02 21:31:14 +0100

    change p to div

--- src/components/Pane.tsx
+++ src/components/Pane.tsx
@@ -143,7 +143,7 @@ function RenderedBody({
 
 function WikiTitle({ title }: { title: string }) {
   return (
-    <p
+    <div
       className="w-screen origin-bottom-left rotate-90 bg-white pb-1.5 text-lg font-medium"
       dangerouslySetInnerHTML={{ __html: title }}
     />
