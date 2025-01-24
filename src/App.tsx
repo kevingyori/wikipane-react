ff5917f329fae9e5f7c182a0536c4236f4ce0fc1
Kevin Gyori <gyorijonatan@gmail.com>
2025-01-24 17:21:39 +0000

    refactor: remove a bunch of deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Panes } from "./components/Panes";
 import { PanesProvider } from "./context/PanesContext";
-
-const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -14,11 +11,9 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PanesProvider>
-        <RouterProvider router={router} />
-      </PanesProvider>
-    </QueryClientProvider>
+    <PanesProvider>
+      <RouterProvider router={router} />
+    </PanesProvider>
   );
 }
 
